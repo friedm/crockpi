f4ac75c2dad4bd2ac886f2bc70fc58ce531d5c61
Matt Friedman <matt@friedm.com>
2016-01-06 00:44:59 +0000

    add controller callback for recording data

--- src/crockpi/controller.py
+++ src/crockpi/controller.py
@@ -1,21 +1,23 @@
 import time, datetime
 from powersupply import PowerSupply
 from tempsensor import TempSensor
-from web import db, models
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-Session = sessionmaker()
-Session.configure(bind=create_engine('sqlite:///crockpi.db'))
-db_session = Session()
+#from web import db, models
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import sessionmaker
+#
+#Session = sessionmaker()
+#Session.configure(bind=create_engine('sqlite:///../crockpi.db'))
+#db_session = Session()
 
 class Controller:
-    def __init__(self,values=None):
+    def __init__(self,values=None,controller_started=None,measurement_taken=None):
         self.__sensor = TempSensor()
         self.__values = values
         self.__start_time = time.time()
         self.__stop = False
         self.__session = None
+        self.__controller_started = controller_started
+        self.__measurement_taken = measurement_taken
 
     def run(self, target_temp):
         """
@@ -30,31 +32,34 @@ class Controller:
         """
 
         print("starting temp controller for", target_temp, "degrees fahrenheit")
-        self.write_session()
+        if self.__controller_started:
+            self.__session = self.__controller_started(datetime.datetime.utcnow(), target_temp)
 
         with PowerSupply() as supply:
             self.regulate(target_temp, supply)
 
-    def write_session(self):
-        self.__session = models.ControlSession(time=datetime.datetime.utcnow())
-        db_session.add(self.__session)
-        db_session.commit()
+#    def write_session(self):
+#        self.__session = models.ControlSession(time=datetime.datetime.utcnow())
+#        db_session.add(self.__session)
+#        db_session.commit()
 
     def regulate(self, target_temp, supply):
         while not self.__stop:
             time.sleep(3)
 
+            time_since_start = time.time() - self.__start_time
             actual_temp = self.__sensor.read()
             print("actual temp:", actual_temp)
 
-            supply.set(target_temp > actual_temp)
-            time_since_start = time.time() - self.__start_time
-            data = models.Data(session=self.__session,
-                    seconds_since_start=time_since_start,
-                    value=self.__sensor.read())
-
-            db_session.add(data)
-            db_session.commit()
+            if self.__measurement_taken:
+                self.__measurement_taken(self.__session, time_since_start, measurement_taken)
+#            supply.set(target_temp > actual_temp)
+#            data = models.Data(session=self.__session,
+#                    seconds_since_start=time_since_start,
+#                    value=self.__sensor.read())
+#
+#            db_session.add(data)
+#            db_session.commit()
 
     def stop(self):
         print("stopping controller...")
